d86ad8780b10fa052063bc3db3c8182147937ca3
Kyle David <kbdavid15@gmail.com>
2017-05-08 23:44:58 -0400

    octopart API is working

--- python/octopart-add/octopart-add/octopart_add.py
+++ python/octopart-add/octopart-add/octopart_add.py
@@ -1,5 +1,30 @@
 import requests
+import json
 
-octopart_api_key = '0c9d5f73'
+url = 'http://octopart.com/api/v3/parts/match?'
 
-print(octopart_api_key)
+mpn = '04025A560FAT2A'
+
+query = [{'mpn':mpn}]
+
+params = {
+    'queries':json.dumps(query),
+    'apikey':'0c9d5f73',
+    'include[]':'specs',
+}
+
+spec_list = ['case_package', 'voltage_rating_dc', 'dielectric_characteristic', 'capacitance_tolerance', 'pin_count', 'capacitance']
+
+
+r = requests.get(url, params)
+response = r.json()
+
+print(response['msec'])
+
+# print mpn's
+for result in response['results']:
+    for item in result['items']:
+        print(item['mpn'])
+        for spec in item['specs']:
+            print(spec)
+    break
