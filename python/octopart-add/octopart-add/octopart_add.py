f4de9db63f26f01c5597c0a8126ced8866269f83
Kyle David <kbdavid15@gmail.com>
2017-05-10 13:11:05 -0400

    added class to handle parts and function to output to file

--- python/octopart-add/octopart-add/octopart_add.py
+++ python/octopart-add/octopart-add/octopart_add.py
@@ -1,9 +1,27 @@
 import requests
 import json
 import time
+import codecs
 
 MAX_QUERIES_PER_REQUEST = 20
 OCTOPART_API_KEY = ''
+
+class Part():
+    def __init__(self, part_match_result):
+        self.part_match_result = part_match_result
+        self.mpn = ""
+        self.url = ""
+        self.manufacturer = ""
+        self.specs = {}
+        self.process_result()
+
+    def process_result(self):
+        first_item = self.part_match_result['items'][0]
+        self.mpn = first_item['mpn']
+        self.url = first_item['octopart_url']
+        self.manufacturer = first_item['manufacturer']['name']
+        self.specs = first_item['specs']
+
 
 def chunks(l, n):
     """Yield successive n-sized chunks from l."""
@@ -33,7 +51,10 @@ def get_specs_from_octopart(mpn_list):
     "Takes a list of mpn strings, and gets the specs from octopart. Returns dict of information"
     match_url = 'http://octopart.com/api/v3/parts/match?'
     results = []
+    idx = 1
     for chunk in chunks(mpn_list, MAX_QUERIES_PER_REQUEST):
+        print('Processing parts {} to {} of {}...'.format(idx, idx+len(chunk)-1, len(mpn_list)))
+        idx += len(chunk)
         query = []
         for mpn in chunk:
             query.append({'mpn':mpn})
@@ -44,27 +65,44 @@ def get_specs_from_octopart(mpn_list):
             'include[]':'specs',
         }
 
-        r = request_handle_timeout(url, params)
+        r = request_handle_timeout(match_url, params)
         if r.ok:
             response = r.json()
             for result in response['results']:
                 results.append(result)  # type is PartsMatchResult object
 
-    return results
+    print("API requests complete")
+    parts = [Part(p) for p in results]
+    return parts
+
+def output_parts_list(parts_list, out_file = "output.csv"):
+    # get list of all possible keys
+    key_list = []
+    for part in parts_list:
+        key_list += part.specs.keys()
+    key_list = list(set(key_list))  # remove duplicates
+
+    with open(out_file, 'w', encoding='utf16') as f:
+        f.write('mpn\tmanufacturer\t')
+        f.write('\t'.join(key_list) + '\n')
+        for part in parts_list:
+            f.write('\t'.join([part.mpn, part.manufacturer]) + '\t')
+            for key in key_list:
+                if key in part.specs.keys():
+                    f.write(part.specs[key]['display_value'] + '\t')
+                else:
+                    f.write('\t')
+            f.write('\n')
+
 
 if __name__ == '__main__':
     mpn_test_list = ['RK73H1ET*3302F', 'RK73H1ET*4751F', 'RK73H1ET*3651F', 'RK73B1JT*824J', 'CRCW0603820KJN*', 'RK73H1ET*2552F', 'RK73H1ET*1213F', 'RK73B1ET*105J', 'RK73H2AT*2552F', 'RK73H1ET*33R0F', 'RK73H1ET*8061F', 'RK73H1ET*3321F', 'RK73H2AT*6651F', 'RK73H1ET*9310F', 'RK73H2AT*6491F', 'RK73H2AT*1001F', 'RK73B1ET*472J', 'CRCW04024K70JN*', 'RK73H1ET*1003F', 'RK73H1ET*9102F', 'RK73H1ET*2201F', 'RK73H1ET*4752F', 'CRCW040247K5FK*', 'RK73H2AT*2611F', 'RK73H1ET*1601F', 'RK73H1ET*6652F', 'RK73H1ET*1152F', 'RK73H1ET*2701F', 'CRCW04022K70FK*' ]
 
     spec_list = ['case_package', 'voltage_rating_dc', 'dielectric_characteristic', 'capacitance_tolerance', 'pin_count', 'capacitance']
 
-    response = get_specs_from_octopart(mpn_test_list[0:5])
+    response = get_specs_from_octopart(mpn_test_list)
 
     # print mpn's
-    for part in response:
-        for item in part['items']:
-            print(item['mpn'])
-            for spec, value in item['specs'].items():
-                print(spec + ' ' + value['display_value'])
-            break
-
-
+    print('Creating output file...')
+    output_parts_list(response)
+    print('Complete')
