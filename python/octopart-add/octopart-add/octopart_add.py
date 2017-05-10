5050c426f8acd009b127062c70382c822d85ab9e
Kyle David <kbdavid15@gmail.com>
2017-05-10 00:05:43 -0400

    fixed printing

--- python/octopart-add/octopart-add/octopart_add.py
+++ python/octopart-add/octopart-add/octopart_add.py
@@ -11,6 +11,7 @@ def chunks(l, n):
         yield l[i:i + n]
 
 def get_api_key():
+    global OCTOPART_API_KEY
     if OCTOPART_API_KEY == '':
         with open('key.txt') as f:
             content = f.readlines()
@@ -52,22 +53,18 @@ def get_specs_from_octopart(mpn_list):
     return results
 
 if __name__ == '__main__':
-    url = 'http://octopart.com/api/v3/parts/match?'
-
-    mpn = '04025A560FAT2A'
     mpn_test_list = ['RK73H1ET*3302F', 'RK73H1ET*4751F', 'RK73H1ET*3651F', 'RK73B1JT*824J', 'CRCW0603820KJN*', 'RK73H1ET*2552F', 'RK73H1ET*1213F', 'RK73B1ET*105J', 'RK73H2AT*2552F', 'RK73H1ET*33R0F', 'RK73H1ET*8061F', 'RK73H1ET*3321F', 'RK73H2AT*6651F', 'RK73H1ET*9310F', 'RK73H2AT*6491F', 'RK73H2AT*1001F', 'RK73B1ET*472J', 'CRCW04024K70JN*', 'RK73H1ET*1003F', 'RK73H1ET*9102F', 'RK73H1ET*2201F', 'RK73H1ET*4752F', 'CRCW040247K5FK*', 'RK73H2AT*2611F', 'RK73H1ET*1601F', 'RK73H1ET*6652F', 'RK73H1ET*1152F', 'RK73H1ET*2701F', 'CRCW04022K70FK*' ]
 
     spec_list = ['case_package', 'voltage_rating_dc', 'dielectric_characteristic', 'capacitance_tolerance', 'pin_count', 'capacitance']
 
-    response = get_specs_from_octopart(mpn_test_list)
-
+    response = get_specs_from_octopart(mpn_test_list[0:5])
 
     # print mpn's
     for part in response:
         for item in part['items']:
             print(item['mpn'])
-            for spec, value in item['specs']:
-                print(spec + ' ' + item['specs'][spec]['display_value'])
+            for spec, value in item['specs'].items():
+                print(spec + ' ' + value['display_value'])
             break
 
 
