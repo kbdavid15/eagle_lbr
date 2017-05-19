68399a9b84d789b93b354f5d78ae169e57491bcb
Kyle David <kyledavid@Kyles-iMac.local>
2017-05-18 21:04:13 -0400

    Modified tk dialog

--- python/octopart-add/octopart-add/eagle-add-part.py
+++ python/octopart-add/octopart-add/eagle-add-part.py
@@ -7,16 +7,27 @@ Utilizes the octopart.com API
 @author Kyle David <kbdavid15@gmail.com
 """
 
-# from eaglepy import default_layers, eagle
+import eaglepy
 import tkinter as tk
+import tkinter.filedialog as fd
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
         self.create_widgets()
+        self.lib_file_name = ''
 
     def create_widgets(self):
+        lib_label = tk.Label(self)
+        lib_label['text'] = "Select Eagle library to add part to"
+        lib_label.pack()
+        self.eagle_lib = tk.Entry()
+        self.eagle_lib.pack(fill=tk.X)
+        self.open_eagle_lib = tk.Button()
+        self.open_eagle_lib['text'] = "Open"
+        self.open_eagle_lib['command'] = self.select_eagle_lib
+        self.open_eagle_lib.pack(anchor='e')
         mpn_label = tk.Label(self)
         mpn_label["text"] = "Manufacturer Part Number"
         mpn_label.pack()
@@ -30,6 +41,9 @@ class Application(tk.Frame):
     def add_parts(self):
         print(f"Now adding {self.mpn_text.get()} to database", )
 
+    def select_eagle_lib(self):
+        self.lib_file_name = fd.askopenfilename()
+        self.eagle_lib.insert(0, self.lib_file_name)
 
 if __name__ == "__main__":
     root = tk.Tk()
